eceb450d314102be3c9dea4d1603bf339077138a
IsaacP <isaac@pachon.com>
2023-11-27 15:19:44 -0500

    Suavizacion Exponencial

--- FinalProyect.py
+++ FinalProyect.py
@@ -13,7 +13,7 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 from darts import TimeSeries
-from darts.models import FFT
+from darts.models import FFT, ExponentialSmoothing
 from darts.metrics import mae
 import warnings
 warnings.filterwarnings("ignore")
@@ -29,6 +29,10 @@ df = df[['Date','Value']]
 df['Date'] = pd.to_datetime(df['Date'],format = '%Y-%m-%d')
 df.head()
 
+#Suaviation exponential of the column Value
+df['Value'] = df['Value'].ewm(alpha=0.1 ,adjust=False).mean()
+
+
 #Creating time series
 #df['Date'] = df['Date'].asfreq('W')
 series = TimeSeries.from_dataframe(df,
@@ -37,6 +41,9 @@ series = TimeSeries.from_dataframe(df,
                                    fill_missing_dates=True, freq='D')
 
 series.head()
+
+
+
 
 #data visualization
 train, val = series.split_before(0.9)
